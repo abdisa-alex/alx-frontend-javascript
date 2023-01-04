1efe651787a47174160f10e44786164654ba1f83
Abdisa Alemu <abdisa.alemu.wako@gmail.com>
2023-01-04 13:18:25 +0300

    Update 1-get_list_student_ids.js

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,7 +1,7 @@
 export default function getListStudentIds(lst) {
-    if (Array.isArray(lst)) {
-      return lst.map((obj) => obj.id);
-    }
-  
-    return [];
+  if (Array.isArray(lst)) {
+    return lst.map((obj) => obj.id);
+  }
+
+  return [];
 }
